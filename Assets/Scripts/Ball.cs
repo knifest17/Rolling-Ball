7a5d8f5955174037b596c1fa042c9d65e3f39359
Knifest <knifest17@gmail.com>
2022-09-02 22:08:43 +0300

    Smoke Effects

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,184 +1,151 @@
 using System.Collections;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] LineRenderer lineRenderer;
-    [SerializeField] LayerMask groundLayer;
-    [SerializeField]
-    float
-        speedMultiplier,
-        friction,
-        squashSpeed,
-        squashSpeedMult,
-        minSquashSpeed,
-        maxSquash;
-    [SerializeField] Transform visibleObject;
-
-    Camera cam;
-    float speed;
-    Vector3 direction;
-    Coroutine movingCoroutine;
-    Rigidbody rb;
-    int lastWallId = 0;
-    float rollingSpeedMult = 1f;
-    
-
-    void Awake()
+    public class Ball : MonoBehaviour
     {
-        cam = Camera.main;
-        rb = GetComponent<Rigidbody>();
-    }
+        [SerializeField] LineRenderer lineRenderer;
+        [SerializeField] LayerMask groundLayer;
+        [SerializeField] BallSettings ballSettings;
+        [SerializeField] Transform visibleObject, particles;
+        [SerializeField] ParticleSystem[] smokeParticles;
+        [SerializeField] Cinemachine.CinemachineVirtualCamera virtualCum;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) OnTouchBegin();
-        if (!Input.GetMouseButton(0) && !Input.GetMouseButtonUp(0)) return;
-        var ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, groundLayer))
+        Camera cam;
+        float speed;
+        Vector3 direction;
+        Coroutine movingCoroutine;
+        Rigidbody rb;
+        int lastWallId = 0;
+        float rollingSpeedMult = 1f;
+        float cameraFOVIncr = 0f;
+
+        void Awake()
         {
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, hit.point);
-            if (Input.GetMouseButtonUp(0))
+            cam = Camera.main;
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void Update()
+        {
+            virtualCum.m_Lens.FieldOfView = 60 + cameraFOVIncr;
+            if (cameraFOVIncr != speed)
             {
-                speed = Mathf.Sqrt(Vector3.Distance(transform.position, hit.point)) * speedMultiplier;
-                direction = (hit.point - transform.position).normalized;
-                direction.y = 0;
-                OnTouchEnd();
+                cameraFOVIncr += Time.deltaTime * (cameraFOVIncr < speed ? 1 : -1) * speed;
+                if (Mathf.Abs(speed - cameraFOVIncr) < Time.deltaTime) cameraFOVIncr = speed;
+            }
+            if (Input.GetMouseButtonDown(0)) OnTouchBegin();
+            if (!Input.GetMouseButton(0) && !Input.GetMouseButtonUp(0)) return;
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, groundLayer))
+            {
+                lineRenderer.SetPosition(0, transform.position);
+                lineRenderer.SetPosition(1, hit.point);
+                if (Input.GetMouseButtonUp(0))
+                {
+                    speed = Mathf.Sqrt(Vector3.Distance(transform.position, hit.point)) * ballSettings.SpeedMultiplier;
+                    direction = (hit.point - transform.position).normalized;
+                    direction.y = 0;
+                    OnTouchEnd();
+                }
             }
         }
-    }
 
-    void OnTouchBegin()
-    {
-        lineRenderer.gameObject.SetActive(true);
-    }
-    IEnumerator MovingRoutine()
-    {
-        while (speed > 0)
+        void OnTouchBegin()
         {
-            transform.position += direction * speed * rollingSpeedMult * Time.deltaTime;
-            visibleObject.RotateAround(
-                visibleObject.position,
-                new Vector3(direction.z, 0, -direction.x),
-                speed * 0.5f * rollingSpeedMult);
-            speed -= friction * rollingSpeedMult * Time.deltaTime;
-            yield return null;
+            lineRenderer.gameObject.SetActive(true);
         }
-    }
-
-    void OnTouchEnd()
-    {
-        if (movingCoroutine is not null) StopCoroutine(movingCoroutine);
-        lineRenderer.gameObject.SetActive(false);
-        movingCoroutine = StartCoroutine(MovingRoutine());
-        lastWallId = 0;
-    }
-
-    void StopMoving()
-    {
-        StopCoroutine(movingCoroutine);
-        movingCoroutine = null;
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.GetInstanceID() == lastWallId) return;
-        if (collision.contactCount == 0) return;
-        lastWallId = collision.gameObject.GetInstanceID();
-        var contact = collision.GetContact(0);
-        var newDirection = Vector3.Reflect(direction, contact.normal);
-        if (speed > minSquashSpeed) StartCoroutine(CollisionRoutine());
-        else direction = newDirection;
-
-        IEnumerator CollisionRoutine()
+        IEnumerator RollingRoutine()
         {
-            rollingSpeedMult = 0.5f;
-            rb.isKinematic = true;
-
-            var lookRot = Quaternion.LookRotation(contact.normal, Vector3.up);
-            transform.rotation = lookRot;
-            visibleObject.Rotate(Vector3.up, -lookRot.eulerAngles.y);
-
-            float compression = 0;
-            float sign = 1f;
-            bool squash = true;
-            float angle = Vector3.Angle(newDirection, contact.normal);
-            float compSpeed = squashSpeed * (Mathf.Pow(angle, 1f / 2f) + 10f) * 0.1f;
-            print(speed);
-            float targetCompression = Mathf.Clamp(0.25f * speed * 0.1f, 0, maxSquash);
-            while (squash && speed > 0)
+            while (speed > 0)
             {
-                transform.position += direction * compSpeed * Time.deltaTime;
-                compression += sign * compSpeed * Time.deltaTime;
-                float delta = compression * 2;
-                transform.localScale = new Vector3(1f + delta, 1f + delta, 1f - delta);
-                if (compression >= targetCompression)
-                {
-                    direction = newDirection;
-                    sign = -1f;
-                }
-                if (sign == -1f && compression <= 0f) squash = false;
+                transform.position += direction * speed * rollingSpeedMult * Time.deltaTime;
+                visibleObject.RotateAround(
+                    visibleObject.position,
+                    new Vector3(direction.z, 0, -direction.x),
+                    speed * 0.35f * rollingSpeedMult);
+                speed -= ballSettings.Friction * rollingSpeedMult * Time.deltaTime;
                 yield return null;
             }
+        }
 
-            transform.localScale = Vector3.one;
-            visibleObject.Rotate(Vector3.up, lookRot.eulerAngles.y);
-            transform.localRotation = Quaternion.identity;
+        void OnTouchEnd()
+        {
+            if (movingCoroutine is not null) StopCoroutine(movingCoroutine);
+            lineRenderer.gameObject.SetActive(false);
+            movingCoroutine = StartCoroutine(RollingRoutine());
+            lastWallId = 0;
+        }
 
-            rb.isKinematic = false;
-            rollingSpeedMult = 1f;
+        void StopMoving()
+        {
+            StopCoroutine(movingCoroutine);
+            movingCoroutine = null;
+        }
+
+        void PlaySmoke()
+        {
+            foreach (var s in smokeParticles)
+            {
+                s.Play();
+            }
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.GetInstanceID() == lastWallId) return;
+            if (collision.contactCount == 0) return;
+            lastWallId = collision.gameObject.GetInstanceID();
+            var contact = collision.GetContact(0);
+            var newDirection = Vector3.Reflect(direction, contact.normal);
+            if (speed > ballSettings.MinSquashSpeed) StartCoroutine(CollisionRoutine());
+            else direction = newDirection;
+            particles.position = contact.point;
+            particles.rotation = Quaternion.LookRotation(contact.normal, Vector3.up);
+            PlaySmoke();
+
+            IEnumerator CollisionRoutine()
+            {
+                rollingSpeedMult = ballSettings.SquashRollingMult;
+                rb.isKinematic = true;
+
+                var lookRot = Quaternion.LookRotation(contact.normal, Vector3.up);
+                transform.rotation = lookRot;
+                visibleObject.Rotate(Vector3.up, -lookRot.eulerAngles.y);
+
+                float compression = 0;
+                float sign = 1f;
+                bool squash = true;
+                float angle = Vector3.Angle(newDirection, contact.normal);
+                float compSpeed = ballSettings.SquashSpeed * (Mathf.Pow(angle, 1f / 2f) + 10f) * 0.1f;
+                print(compSpeed);
+                float targetCompression = Mathf.Clamp(0.25f * speed * 0.1f, 0, ballSettings.MaxSquash);
+                var material = visibleObject.GetComponent<MeshRenderer>().material;
+                while (squash && speed > 0)
+                {
+                    transform.position += direction * compSpeed * Time.deltaTime;
+                    compression += sign * compSpeed * Time.deltaTime;
+                    float delta = compression * 2;
+                    transform.localScale = new Vector3(1f + delta, 1f + delta, 1f - delta);
+                    material.color = Color.Lerp(Color.white, Color.red, compression / ballSettings.MaxSquash * 0.5f);
+                    if (compression >= targetCompression)
+                    {
+                        direction = newDirection;
+                        sign = -1f;
+                    }
+                    if (sign == -1f && compression <= 0f) squash = false;
+                    yield return null;
+                }
+
+                transform.localScale = Vector3.one;
+                visibleObject.Rotate(Vector3.up, lookRot.eulerAngles.y);
+                transform.localRotation = Quaternion.identity;
+
+                rb.isKinematic = false;
+                rollingSpeedMult = 1f;
+            }
         }
     }
-
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.GetInstanceID() == lastWallId) return;
-    //    if (collision.contactCount == 0) return;
-    //    lastWallId = collision.gameObject.GetInstanceID();
-    //    var contact = collision.GetContact(0);
-    //    var newDirection = Vector3.Reflect(direction, contact.normal);
-    //    if (speed > minSquashSpeed) StartCoroutine(CollisionRoutine());
-    //    else direction = newDirection;
-
-    //    IEnumerator CollisionRoutine()
-    //    {
-    //        if (movingCoroutine != null) StopMoving();
-    //        rb.isKinematic = true;
-
-    //        var lookRot = Quaternion.LookRotation(contact.normal, Vector3.up);
-    //        transform.rotation = lookRot;
-    //        visibleObject.Rotate(Vector3.up, -lookRot.eulerAngles.y);
-
-    //        float compression = 0;
-    //        float sign = 1f;
-    //        bool squash = true;
-    //        float angle = Vector3.Angle(newDirection, contact.normal);
-    //        float squashSpeed = (1 / speed * squashSpeedMult) * (angle + 10f) * 0.1f;
-    //        print(speed);
-    //        float maxCompression = 0.25f * speed * 0.1f;
-    //        while (squash)
-    //        {
-    //            transform.position += direction * squashSpeed * Time.deltaTime;
-    //            float delta = compression * 2;
-    //            transform.localScale = new Vector3(1f + delta, 1f + delta, 1f - delta);
-    //            compression += sign * squashSpeed * Time.deltaTime;
-    //            if (compression >= maxCompression)
-    //            {
-    //                direction = newDirection;
-    //                sign = -1f;
-    //            }
-    //            if (sign == -1f && compression <= 0f) squash = false;
-    //            yield return null;
-    //        }
-
-    //        transform.localScale = Vector3.one;
-    //        visibleObject.Rotate(Vector3.up, lookRot.eulerAngles.y);
-    //        transform.localRotation = Quaternion.identity;
-
-    //        rb.isKinematic = false;
-    //        movingCoroutine = StartCoroutine(MovingRoutine());
-    //    }
-    //}
 }
---
+++ Assets/Scripts/BallSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "BallSettings", menuName = "BallSettings", order = 0)]
+    public class BallSettings : ScriptableObject
+    {
+        [SerializeField]
+        float
+            speedMultiplier,
+            friction,
+            squashSpeed,
+            squashSpeedMult,
+            squashRollingMult,
+            minSquashSpeed,
+            maxSquash;
+
+        public float SpeedMultiplier => speedMultiplier;
+        public float Friction => friction;
+        public float SquashSpeed => squashSpeed;
+        public float SquashSpeedMult => squashSpeedMult;
+        public float SquashRollingMult => squashRollingMult;
+        public float MinSquashSpeed => minSquashSpeed;
+        public float MaxSquash => maxSquash;
+    }
+}
